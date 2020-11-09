7171cace4d6ffc51717e17f3df047e39d80ab07f
Takeru <takeru.moe@gmail.com>
2020-11-09 16:39:46 +0800

    fix(preloadImage): use absolute url

--- src/extension/preloadImage.js
+++ src/extension/preloadImage.js
@@ -1,7 +1,7 @@
 import { each, keys } from "../include/zeta/util.js";
 import { isCssUrlValue } from "../include/zeta/cssUtil.js";
 import { preloadImages } from "../util/common.js";
-import { withBaseUrl } from "../util/path.js";
+import { toAbsoluteUrl } from "../util/path.js";
 import { isElementActive } from "./router.js";
 import { install } from "../app.js";
 import defaults from "../defaults.js";
@@ -16,14 +16,14 @@ install('preloadImage', function (app) {
         each(domUpdates, function (element, props) {
             if ((props.src || props.style) && isElementActive(element)) {
                 if (props.src) {
-                    urls[props.src] = true;
+                    urls[toAbsoluteUrl(props.src)] = true;
                 }
                 if (props.style) {
                     each(IMAGE_STYLE_PROPS, function (i, v) {
                         // @ts-ignore: props.style checked for truthiness
                         var imageUrl = isCssUrlValue(props.style[v]);
                         if (imageUrl) {
-                            urls[withBaseUrl(imageUrl)] = true;
+                            urls[toAbsoluteUrl(imageUrl)] = true;
                         }
                     });
                 }
