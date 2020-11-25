89160c8a54a805def05c7553aa297481785fd110
Takeru <takeru.moe@gmail.com>
2020-11-26 01:05:14 +0800

    chore: update dev server

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const basepath = __dirname;
 
 server.use('/node_modules', express.static('node_modules'));
 try {
-    const peerIncludePath = path.resolve(basepath, '../zeta-dom/src');
+    const peerIncludePath = path.resolve(basepath, '../zeta-dom');
     fs.lstatSync(peerIncludePath);
-    server.use('/zeta-dom', express.static(peerIncludePath));
+    server.use('/zeta-dom/dist', express.static(path.join(peerIncludePath, 'dist')));
+    server.use('/zeta-dom', express.static(path.join(peerIncludePath, 'src')));
 } catch (e) {
     server.use('/zeta-dom', express.static('node_modules/zeta-dom'));
 }
-server.use('/dev', express.static(path.resolve(basepath, 'dev')));
 server.get('/src/include/zeta/*', function (req, res) {
     console.log('GET', req.path);
     fs.readFile(path.join(__dirname, req.path), function (err, data) {
@@ -30,6 +30,8 @@ server.get('/src/include/zeta/*', function (req, res) {
     });
 });
 server.use('/src', express.static('src'));
+server.use('/dev', express.static('dev'));
+server.use('/dist', express.static('dist'));
 
 const port = process.env.PORT || 3000;
 server.listen(port);
