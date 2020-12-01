78a9e4af9f2f6c00d108a9728c35a87a86cde03c
Takeru <takeru.moe@gmail.com>
2020-12-01 16:49:20 +0800

    chore: update dev server

--- server.cjs
+++ server.cjs
@@ -5,15 +5,25 @@ const express = require('express');
 const server = express();
 const basepath = __dirname;
 
+const zetaIncludePath = (function () {
+    var realPath = path.resolve(basepath, '../zeta-dom/src');
+    return fs.existsSync(realPath) ? realPath : path.resolve(basepath, 'node_modules/zeta-dom');
+})();
+
+server.get('**/*.cjs', function (req, res) {
+    var realPath = /^\/zeta-dom\/(.+)$/.test(req.path) ? path.join(zetaIncludePath, RegExp.$1) : path.join(basepath, req.path);
+    fs.readFile(realPath, function (err, data) {
+        console.log('GET', req.path, '->', realPath);
+        if (err) {
+            res.sendStatus(404);
+        } else {
+            res.contentType('application/javascript');
+            res.send(data.toString().replace(/module\.exports\s*=\s*/g, 'export default '));
+        }
+    });
+});
 server.use('/node_modules', express.static('node_modules'));
-try {
-    const peerIncludePath = path.resolve(basepath, '../zeta-dom');
-    fs.lstatSync(peerIncludePath);
-    server.use('/zeta-dom/dist', express.static(path.join(peerIncludePath, 'dist')));
-    server.use('/zeta-dom', express.static(path.join(peerIncludePath, 'src')));
-} catch (e) {
-    server.use('/zeta-dom', express.static('node_modules/zeta-dom'));
-}
+server.use('/zeta-dom', express.static(zetaIncludePath));
 server.get('/src/include/zeta/*', function (req, res) {
     console.log('GET', req.path);
     fs.readFile(path.join(__dirname, req.path), function (err, data) {
