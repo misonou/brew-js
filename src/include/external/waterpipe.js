d7d6426e96321224ce1a9c7463c76ea9d295cec2
Takeru <takeru.moe@gmail.com>
2021-04-01 12:21:15 +0800

    feat(template): object literal self reference

--- src/include/external/waterpipe.js
+++ src/include/external/waterpipe.js
@@ -7,7 +7,10 @@ module.exports = waterpipe;
 // assign to a new variable to avoid incompatble declaration issue by typescript compiler
 const waterpipe_ = waterpipe;
 waterpipe_.pipes['{'] = function (_, varargs) {
+    var globals = varargs.globals;
+    var prev = globals.new;
     var o = {};
+    globals.new = o;
     while (varargs.hasArgs()) {
         var key = varargs.raw();
         if (key === '}') {
@@ -15,6 +18,7 @@ waterpipe_.pipes['{'] = function (_, varargs) {
         }
         o[String(key).replace(/:$/, '')] = varargs.next();
     }
+    globals.new = prev;
     return o;
 };
 // @ts-ignore: add member to function
