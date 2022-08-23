26c15bb2d91f03e665c6fe39c77d8b08a3b3c56e
Takeru <takeru.moe@gmail.com>
2022-08-23 19:02:50 +0800

    fix(viewport): use visualViewport if available

--- src/extension/viewport.js
+++ src/extension/viewport.js
@@ -1,5 +1,6 @@
 import $ from "../include/external/jquery.js";
 import { define, defineObservableProperty, either, setTimeoutOnce } from "../include/zeta-dom/util.js";
+import { bind } from "../include/zeta-dom/domUtil.js";
 import { IS_TOUCH } from "../include/zeta-dom/env.js";
 import dom from "../include/zeta-dom/dom.js";
 import { animateIn } from "../anim.js";
@@ -7,15 +8,21 @@ import { addExtension } from "../app.js";
 
 export default addExtension(true, 'viewport', function (app) {
     var setOrientation = defineObservableProperty(app, 'orientation', '', true);
+    var visualViewport = window.visualViewport;
     var useAvailOrInner = IS_TOUCH && navigator.platform !== 'MacIntel';
     var aspectRatio, viewportWidth, viewportHeight;
 
     function checkViewportSize(triggerEvent) {
-        var availWidth = screen.availWidth;
-        var availHeight = screen.availHeight;
+        if (visualViewport) {
+            viewportWidth = visualViewport.width;
+            viewportHeight = visualViewport.height;
+        } else {
+            var availWidth = screen.availWidth;
+            var availHeight = screen.availHeight;
+            viewportWidth = useAvailOrInner ? availWidth : document.body.offsetWidth;
+            viewportHeight = useAvailOrInner ? (availWidth === window.innerWidth ? availHeight : window.innerHeight) : document.body.offsetHeight;
+        }
         var previousAspectRatio = aspectRatio;
-        viewportWidth = useAvailOrInner ? availWidth : document.body.offsetWidth;
-        viewportHeight = useAvailOrInner ? (availWidth === window.innerWidth ? availHeight : window.innerHeight) : document.body.offsetHeight;
         aspectRatio = viewportWidth / viewportHeight;
         setOrientation(aspectRatio >= 1 ? 'landscape' : 'portrait');
 
@@ -48,10 +55,15 @@ export default addExtension(true, 'viewport', function (app) {
     app.on('orientationchange', function () {
         animateIn(dom.root, 'orientationchange');
     });
-    $(window).on('resize', function () {
-        setTimeoutOnce(checkViewportSize);
-    });
-    $(function () {
+    if (visualViewport) {
+        bind(visualViewport, 'resize', checkViewportSize);
         checkViewportSize(false);
-    });
+    } else {
+        $(window).on('resize', function () {
+            setTimeoutOnce(checkViewportSize);
+        });
+        $(function () {
+            checkViewportSize(false);
+        });
+    }
 });
