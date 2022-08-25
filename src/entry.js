c79b380495a737048cdd053582589acdbc74fbad
Takeru <takeru.moe@gmail.com>
2022-08-25 16:59:11 +0800

    fix(entry): export router extension to umd module

--- src/entry.js
+++ src/entry.js
@@ -7,9 +7,11 @@ import lib4 from "./extension/login.js";
 import lib5 from "./extension/preloadImage.js";
 import lib6 from "./extension/scrollable.js";
 import lib7 from "./extension/viewport.js";
+import lib8 from "./extension/router.js";
+import lib9 from "./extension/htmlRouter.js";
 
 function exportAppToGlobal(app) {
     window.app = app;
 }
 
-export default brew.with(exportAppToGlobal, lib1, lib2, lib3, lib4, lib5, lib6, lib7);
+export default brew.with(exportAppToGlobal, lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9);
