c2782ba475e6dea334b5776c44165b697f98c2a8
Takeru <takeru.moe@gmail.com>
2025-02-12 18:49:34 +0800

    chore: polyfill only when needed

--- src/include/waterpipe.js
+++ src/include/waterpipe.js
@@ -3,19 +3,21 @@ export default waterpipe;
 
 // assign to a new variable to avoid incompatble declaration issue by typescript compiler
 const waterpipe_ = waterpipe;
-waterpipe_.pipes['{'] = function (_, varargs) {
-    var globals = varargs.globals;
-    var prev = globals.new;
-    var o = {};
-    globals.new = o;
-    while (varargs.hasArgs()) {
-        var key = varargs.raw();
-        if (key === '}') {
-            break;
+if (!waterpipe_.pipes['{']) {
+    waterpipe_.pipes['{'] = function (_, varargs) {
+        var globals = varargs.globals;
+        var prev = globals.new;
+        var o = {};
+        globals.new = o;
+        while (varargs.hasArgs()) {
+            var key = varargs.raw();
+            if (key === '}') {
+                break;
+            }
+            o[String(key).replace(/:$/, '')] = varargs.next();
         }
-        o[String(key).replace(/:$/, '')] = varargs.next();
-    }
-    globals.new = prev;
-    return o;
-};
-waterpipe_.pipes['{'].varargs = true;
+        globals.new = prev;
+        return o;
+    };
+    waterpipe_.pipes['{'].varargs = true;
+}
